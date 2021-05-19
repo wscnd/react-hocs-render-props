73cf0e1d7a15dcf1e790b8b3a32b0968798ba30d
wscnd <wellingson@gmail.com>
2021-05-19 16:36:26 -0300

    Clean Up and Basic Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import _ from "lodash";
+
+import * as Examples from "./examples";
+
+const Components = [
+  { name: "RenderProp FC", Component: Examples.RenderPropFC },
+];
 
 function App() {
+  const [selection, setSelection] = React.useState(Components[0]);
+  const componentsByName = React.useMemo(() => _.keyBy(Components, "name"), []);
+
+  function onSelectChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    const selectedName = event.target.value;
+    const component = componentsByName[selectedName];
+    setSelection(component);
+  }
+  console.log(componentsByName);
+
+  function stringify(object: any) {
+    return (
+      <pre>
+        <code>{JSON.stringify(object, null, 2)}</code>
+      </pre>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <select name="select-components" id="select-1" onChange={onSelectChange}>
+        {Components.map((c) => (
+          <option key={c.name} value={c.name}>
+            {c.name}
+          </option>
+        ))}
+      </select>
+      <div>
+        {stringify(componentsByName)}
+        {stringify(selection)}
+      </div>
     </div>
   );
 }
---
+++ src/examples/RenderPropFC.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+const RenderPropFC: React.FunctionComponent = () => {
+  return <h1>Hello from App</h1>;
+};
+
+export { RenderPropFC  }
